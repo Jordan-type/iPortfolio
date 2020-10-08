bdc2b5ba6c79249f52e095358d3a0771429418bf
Jordan-type <jordanmuthemba25@gmail.com>
2020-10-08 11:07:54 +0300

    Added procfile

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -9,6 +9,6 @@ $this->title = 'Jordan type';
   <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
     <div class="hero-container" data-aos="fade-in">
       <h1>Jordan type</h1>
-      <p>I'm a <span class="typed" data-typed-items="Designer, Developer, Blockchain Developer, Freelancer, Photographer"></span></p>
+      <p>I'm a <span class="typed" data-typed-items="UI/UX Designer,web Developer, Blockchain Developer, Freelancer"></span></p>
     </div>
   </section><!-- End Hero -->
